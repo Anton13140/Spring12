26de3998d49bf4444868fd2c19961b4e1897ddfb
anton <borisov.cameron@yandex.ru>
2024-03-07 03:56:00 +0500

    d

---
+++ Spring_HW/src/main/java/com/example/springHW/domain/User.java
@@ -0,0 +1,25 @@
+package com.example.springHW.domain;
+
+import lombok.Data;
+
+/**
+ * Обьект пользователь.
+ */
+@Data
+public class User {
+
+    /**
+     * Имя пользователя
+     */
+    private String name;
+
+    /**
+     * Возраст пользователя
+     */
+    private int age;
+    /**
+     * Почтовый адрес пользователя
+     */
+    private String email;
+
+}
